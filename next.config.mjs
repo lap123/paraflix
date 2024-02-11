adb1ed3ae3913a1ce60f3c3d8b47289013d714a5
lap123 <jdairay@gmail.com>
2024-02-11 11:19:45 +0100

    Setup github pages

--- next.config.mjs
+++ next.config.mjs
@@ -7,3 +7,21 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
