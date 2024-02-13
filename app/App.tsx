6d3bb383a9e32de8cb416729a91635414ef832e2
lap123 <jdairay@gmail.com>
2024-02-13 10:53:03 +0100

    Added analytics

--- app/App.tsx
+++ app/App.tsx
@@ -6,6 +6,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ children }: Readonly<{children: React.ReactNode}>) {
     const [queryClient] = useState(new QueryClient())
@@ -14,6 +15,7 @@ export default function App({ children }: Readonly<{children: React.ReactNode}>)
         <QueryClientProvider client={queryClient}>
             {children}
             <ReactQueryDevtools initialIsOpen={false} />
+            <Analytics />
         </QueryClientProvider>
     )
 }
